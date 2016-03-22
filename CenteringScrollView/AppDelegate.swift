a11bae7f26def03c3e3d9ab36db37366ab4ff0ef
Matt Rajca <matt.rajca@me.com>
2016-03-22 10:42:42 -0500

    Cleaned up AppDelegate

--- CenteringScrollView/AppDelegate.swift
+++ CenteringScrollView/AppDelegate.swift
@@ -6,22 +6,10 @@
 //  Copyright © 2016 Matt Rajca. All rights reserved.
 //
 
-import Cocoa
+import AppKit
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
+final class AppDelegate: NSObject, NSApplicationDelegate {
 	@IBOutlet weak var window: NSWindow!
-
-
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
-	}
-
-	func applicationWillTerminate(aNotification: NSNotification) {
-		// Insert code here to tear down your application
-	}
-
-
 }
 
